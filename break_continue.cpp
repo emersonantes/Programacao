d91ac3a313deed17406a691652b55263a94c354c
Emerson Nantes <37250049+emersonantes@users.noreply.github.com>
2024-08-27 09:26:39 -0300

    Create break_continue.cpp

---
+++ break_continue.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	while(true)
+	{
+		n++;
+		if (n>=10)
+		{
+			break;
+		}
+		else
+		{
+			if(n==4 or n==6)
+			{
+				continue;
+			}
+		}
+
+	
+	    cout<<n<<endl;
+	}
+
+	return 0;
+}
